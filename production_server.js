533a5da69e369adc3f1e06bdc66f45e2ecc78fec
Kavish <kavish619@gmail.com>
2019-09-19 00:11:22 +0530

    enabled push-state to print message on the console

--- production_server.js
+++ production_server.js
@@ -1,6 +1,9 @@
 var server = require('pushstate-server')
 
 server.start({
-  port: 8000,
-  directory: './build',
-})
+    port: 8000,
+    host: '127.0.0.1',
+    directory: './build',
+  }, (err, address) =>
+    console.log(`Listening on port ${address.port} (http://${address.address}:${address.port})`)
+)
