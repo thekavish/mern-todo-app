556812cfd7b5fd49dbfdc09d7390c8dde0225aa0
Kavish <kavish@technoscore.net>
2019-09-19 14:48:18 +0530

    added npm scripts to serve backend and CRA-build pushstate now also opens browser

--- production_server.js
+++ production_server.js
@@ -1,9 +1,14 @@
-var server = require('pushstate-server')
+const server = require('pushstate-server')
+const openBrowser = require('react-dev-utils/openBrowser')
 
+console.log(`Starting to serve build...\n`)
 server.start({
     port: 8000,
     host: '127.0.0.1',
     directory: './build',
-  }, (err, address) =>
-    console.log(`Listening on port ${address.port} (http://${address.address}:${address.port})`)
+  }, (err, address) => {
+    const url = `http://${address.address}:${address.port}`
+    console.log(`Listening on port ${address.port} (${url})`)
+    openBrowser(url)
+  },
 )
